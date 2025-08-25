c0018b885a91aa3eb314787a231fec7a2e0dc305
Sam Davenport <157653319+thereclaimer@users.noreply.github.com>
2025-08-24 19:09:30 -0500

    Updating scripts
    streamlining the powershell module and build scripts

--- scripts/SLDLib.psm1
+++ scripts/SLDLib.psm1
@@ -1,76 +1,154 @@
-$Script:BatFiles = [PSCustomObject]@{
-    Debug   = '..\scripts\build-debug.bat'
-    Release = '..\scripts\build-release.bat'
+############################
+# OUTPUT
+############################
+
+$Script:OutFiles = [PSCustomObject]@{
+    Obj = 'SLD.Win32.obj'
+    Pdb = 'SLD.Win32.pdb'
+    Lib = 'SLD.Win32.lib'
 }
 
-$Script:Commands = [PSCustomObject]@{
-    Cl  = "cl.exe"
-    Lib = "lib.exe" 
+$Script:BatFiles = [PSCustomObject]@{
+    Debug   = 'scripts\SLD.Build.Debug.bat'
+    Release = 'scripts\SLD.Build.Release.bat'
+}
+
+$Script:BuildTools = [PSCustomObject]@{
+    Compiler = "cl.exe"
+    Library  = "lib.exe" 
 }
 
 $Script:OutDirsDebug = [PSCustomObject]@{
-    Bin = '..\build\debug\bin'
-    Obj = '..\build\debug\obj'
-    Lib = '..\build\debug\lib'
+    Bin = 'build\debug\bin'
+    Obj = 'build\debug\obj'
+    Lib = 'build\debug\lib'
 }
 
 $Script:OutDirsRelease = [PSCustomObject]@{
-    Bin = '..\build\release\bin'
-    Obj = '..\build\release\obj'
-    Lib = '..\build\release\lib'
+    Bin = 'build\release\bin'
+    Obj = 'build\release\obj'
+    Lib = 'build\release\lib'
 }
 
-$Script:ClIncludeFlags = @(
-    '/I' + '..\include',
-    '/I' + '..\src',
-    '/I' + '..\src\os',
-    '/I' + '..\src\core',
-    '/I' + '..\src\memory',
-    '/I' + '..\src\string',
-    '/I' + '..\src\win32',
-    '/I' + '..\vcpkg_installed\x64-windows\include'
-)
+$Script:OutFilesDebug = [PSCustomObject]@{
+    Obj = ($Script:OutDirsDebug + '\' + $Script:OutFiles.Obj)
+    Pdb = ($Script:OutDirsDebug + '\' + $Script:OutFiles.Pdb)
+    Lib = ($Script:OutDirsDebug + '\' + $Script:OutFiles.Lib)
+}
 
-$Script:ClDebugFlags = @(
-    "/Fo:" + $Script:OutDirsDebug.Obj + "\SLD.Win32.obj", # obj output
-    "/nologo",                                            # startup banner disabled
-    "/c",                                                 # compile without linking
-    "/MD",                                                # link against multithreaded runtime library (MSVCRT.dll)
-    "/Z7",                                                # generate debug info
-    "/EHs-",                                              # disable exception handling
-    "/std:c++17",                                         # c++17 standard mode
-    "/Od",                                                # disable optimizations
-    "/D_HAS_EXCEPTIONS=0"                                 # disable exceptions for STL and CRT
-)
+############################
+# COMPILE
+############################
 
-$Script:ClSourceFile         = Join-Path $PSScriptRoot '..\src\sld.cpp'
-$Script:ClIncludeFlagsString = $Script:ClIncludeFlags -join " "
-$Script:ClDebugFlagsString   = $Script:ClDebugFlags   -join " "
-$Script:ClDebugCommand       = @(
-    $Script:Commands.Cl,
-    $Script:ClSourceFile,
-    $Script:ClIncludeFlagsString,
-    $Script:ClDebugFlagsString
-) -join " " 
+$Script:CompileArgsDebug = [PSCustomObject]@{
+    In      = 'src\sld.cpp'
+    Out     = '/Fo:' + $Script:OutFilesDebug.Obj
+    Include = @(
+        '/I' + 'include'
+        '/I' + 'src'
+        '/I' + 'src\core'
+        '/I' + 'src\memory'
+        '/I' + 'src\os'
+        '/I' + 'src\string'
+        '/I' + 'src\win32'
+        '/I' + 'vcpkg_installed\x64-windows\include'
+    ) -join ' '
+    Flags = @(
+        '/nologo'             # startup banner disabled
+        '/c'                  # compile without linking
+        '/MD'                 # link against multithreaded runtime library (MSVCRT.dll)
+        '/Z7'                 # generate debug info
+        '/EHs-'               # disable exception handling
+        '/std:c++17'          # c++17 standard mode
+        '/Od'                 # disable optimizations
+        '/D_HAS_EXCEPTIONS=0' # disable exceptions for STL and CRT
+    ) -join ' '    
+}
 
-$Script:LibFlags = @(
-    "/nologo",
-    ("/OUT:" + $Script:OutDirsDebug.Lib + "\SLD.Win32.lib"),
-    ($Script:OutDirsDebug.Obj + "\SLD.Win32.obj")
-) -join " "
+$Script:CompileExpressionDebug = @(
+    $Script:BuildTools.Compiler
+    $Script:CompileArgsDebug.In
+    $Script:CompileArgsDebug.Out
+    $Script:CompileArgsDebug.Include
+    $Script:CompileArgsDebug.Flags
+) -join ' '
 
-$Script:LibCommand = @(
-    $Script:Commands.Lib,
-    $Script:LibFlags
-) -join " "
+############################
+# LIBRARY
+############################
 
-function Invoke-SLDDebugBuild {
+$Script:LibraryArgsDebug = [PSCustomObject]@{
+    In    = $Script:OutFilesDebug.Obj
+    Out   = '/OUT:' + $Script:OutFilesDebug.Lib
+    Flags = '/nologo'
+}
 
-    Write-Output $Script:ClDebugCommand
-    Write-Output $Script:LibCommand
+$Script:LibraryExpressionDebug = @(
+    $Script:BuildTools.Library
+    $Script:DebugLibraryArgs.In
+    $Script:DebugLibraryArgs.Out
+    $Script:DebugLibraryArgs.Flags
+) -join ' '
+
+############################
+# BUILD SCRIPT
+############################
+
+$Script:BuildScriptDebug = @"
+@echo off
+
+pushd ..
+
+@set dir_bin=    $($Script:OutDirsDebug.Bin)
+@set dir_lib=    $($Script:OutDirsDebug.Lib)
+@set dir_obj=    $($Script:OutDirsDebug.Obj)
+
+@set cl_in=      $($Script:CompileArgsDebug.In)
+@set cl_out=     $($Script:CompileArgsDebug.Out)
+@set cl_include= $($Script:CompileArgsDebug.Include)
+@set cl_flags=   $($Script:CompileArgsDebug.Flags)
+
+@set lib_in=    $($Script:LibraryArgsDebug.In)
+@set lib_out=   $($Script:LibraryArgsDebug.Out)
+@set lib_flags= $($Script:LibraryArgsDebug.Flags)
+
+@set cmd_cl=    $($Script:BuildTools.Compiler)  %cl_in%  %cl_out%  %cl_include%  %cl_flags%
+@set cmd_lib=   $($Script:BuildTools.Library) %lib_flags% %lib_in% %lib_out%
+
+IF NOT EXIST %dir_bin% mkdir %dir_bin%
+IF NOT EXIST %dir_lib% mkdir %dir_lib%
+IF NOT EXIST %dir_obj% mkdir %dir_obj%
+
+call %cmd_cl%
+call %cmd_link%
+
+popd
+"@
+
+############################
+# FUNCTIONS
+############################
+
+function Invoke-SLDBuildDebug {
+
+    Push-Location (Join-Path $PSScriptRoot '..')
+
+    Export-SLDOutDirsDebug
 
     Invoke-Expression $Script:ClDebugCommand
     Invoke-Expression $Script:LibCommand
+
+    Pop-Location
+}
+
+function Export-SLDBuildDebug {
+
+    Push-Location (Join-Path $PSScriptRoot '..')
+
+    New-Item    -Path $Script:BatFiles.Debug -ItemType File -Force
+    Set-Content -Path $Script:BatFiles.Debug -Value    $Script:BuildScriptDebug
+
+    Pop-Location
 }
 
 function Export-SLDOutDirsDebug {
@@ -87,25 +165,6 @@ function Export-SLDOutDirsRelease {
 	if (!(Test-Path -Path $Script:OutDirsRelease.Lib)) { New-Item -ItemType Directory -Path $Script:OutDirsRelease.Lib }
 }
 
-function Export-SLDBatchScripts {
-
-    # create files
-    if (Test-Path $Script:BatFiles.Debug)   { Remove-Item $Script:BatFiles.Debug   -Force }
-    if (Test-Path $Script:BatFiles.Release) { Remove-Item $Script:BatFiles.Release -Force }
-    New-Item $Script:BatFiles.Debug
-    New-Item $Script:BatFiles.Release
-
-    # build debug
-    ("@echo off")                               | Out-File -Encoding ASCII -Append -FilePath $Script:BatFiles.Debug
-    ("")                                        | Out-File -Encoding ASCII -Append -FilePath $Script:BatFiles.Debug
-    ("@set cmd_cl="   + $Script:ClDebugCommand) | Out-File -Encoding ASCII -Append -FilePath $Script:BatFiles.Debug
-    ("@set cmd_lib="  + $Script:LibCommand)     | Out-File -Encoding ASCII -Append -FilePath $Script:BatFiles.Debug
-    ("")                                        | Out-File -Encoding ASCII -Append -FilePath $Script:BatFiles.Debug
-    ("call %cmd_cl%")                           | Out-File -Encoding ASCII -Append -FilePath $Script:BatFiles.Debug
-    ("call %cmd_lib%")                          | Out-File -Encoding ASCII -Append -FilePath $Script:BatFiles.Debug
-
-}
-
 function Install-SLDDependencies {
 
     $vcpkg = "vcpkg install"
@@ -113,8 +172,8 @@ function Install-SLDDependencies {
     Invoke-Expression $vcpkg
 }
 
-Export-ModuleMember -Function Install-SLDDependencies
-Export-ModuleMember -Function Invoke-SLDDebugBuild
+Export-ModuleMember -Function Invoke-SLDBuildDebug
+Export-ModuleMember -Function Export-SLDBuildDebug
 Export-ModuleMember -Function Export-SLDOutDirsDebug
 Export-ModuleMember -Function Export-SLDOutDirsRelease
-Export-ModuleMember -Function Export-SLDBatchScripts
+Export-ModuleMember -Function Install-SLDDependencies
