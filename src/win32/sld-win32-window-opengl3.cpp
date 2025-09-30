2b4f8d4d56f0b732a55737527350ffaa61d65ebc
thereclaimer <sam.davenport@thereclaimergame.com>
2025-09-30 08:45:37 -0500

    syntax fix

--- src/win32/sld-win32-window-opengl3.cpp
+++ src/win32/sld-win32-window-opengl3.cpp
@@ -176,12 +176,9 @@ namespace sld {
         if (result) return(true);
 
         switch(message) {
-            case (WM_CLOSE):
-            case (WM_DESTROY): {
-                PostQuitMessage(0);
-            } break;
+            case (WM_CLOSE): { PostQuitMessage(0); } break;
             default: break;
-        };\
+        };
 
         result = DefWindowProc(
             handle,
